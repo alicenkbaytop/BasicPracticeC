f513fbb3261d0b1283302bbe00b6d6fcc5260412
Ali Cenk Baytop <55930902+alicenkbaytop@users.noreply.github.com>
2019-12-25 11:56:26 +0300

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+//By ACB
+int fact(int n)
+{
+    if (n>=1)
+    {
+        return n*fact(n-1);    
+    }
+    else if (n == 0)
+    {
+    return 1;
+    }
+    else
+    {
+        return 0;
+    }
+} 
+
+int main()
+{
+    int size, i;
+
+    printf("Enter the element number of the Array(Factorial): \n");
+    scanf("%d", &size);
+    
+    int arr[size];
+    
+    for (i = 0; i < size; i++)
+    {   
+       arr[i] = fact(i);
+    }
+    
+    printf("Array %d equal to %d \n",i,fact(i));
+    
+    return 0;
+}
+
