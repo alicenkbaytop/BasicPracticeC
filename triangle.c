7c529f0147182f69d9232763233c4aa3caaf8bfa
Ali Cenk Baytop <55930902+alicenkbaytop@users.noreply.github.com>
2019-12-28 02:30:59 +0300

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("Enter row number: ");
+	int row;
+	scanf("%d", &row);
+	
+	int i, j;
+	for (i=1; i<=row; i++)
+	{
+		for (j=1; j<=i;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
