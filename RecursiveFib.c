9f0ef482e2ec478616244036a7485a442315ba04
Ali Cenk Baytop <55930902+alicenkbaytop@users.noreply.github.com>
2020-01-21 03:50:43 +0300

    Add files via upload

---
+++ RecursiveFib.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int fib(int n)
+{
+	if (n >= 2)
+	{
+		return fib(n-1) + fib (n-2);
+	}
+}
+
+int main()
+{
+	// first step is taken the 1.
+	int n;
+	printf("Enter a step for fibonacci: \n");
+	scanf("%d", &n);
+	printf("%d",fib(n));
+	
+	return 0;
+}
