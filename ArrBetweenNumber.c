0dcad6590b7ed73f2a51862fbe07d9ce02ac003b
Ali Cenk Baytop <55930902+alicenkbaytop@users.noreply.github.com>
2020-01-21 15:07:45 +0300

    Add files via upload

---
+++ ArrBetweenNumber.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int n1, n2;
+	printf("For example n1-n2: \n");
+	scanf("%d %d", &n1, &n2);
+	
+	int arr1[n2];
+	
+	for (n1; n1 < n2; n1++)
+	{
+		if (n1 % 2 == 1)
+		{
+			arr1[n1] = n1;
+			printf("Array %d equal to %d\n",arr1[n1],n1);
+		}	
+	}
+	return 0;
+}
