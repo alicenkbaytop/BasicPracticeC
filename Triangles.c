ef72a496fb1465d5aeef5105644d4b26b38388c3
Ali Cenk Baytop <55930902+alicenkbaytop@users.noreply.github.com>
2020-01-21 15:34:40 +0300

    Add files via upload

---
+++ Triangles.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (i % 2 == 1)
+			{
+				printf("*");
+			}
+			else 
+			{
+					printf("%d",j);
+				
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
