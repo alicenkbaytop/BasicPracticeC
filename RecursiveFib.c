2109bbf6831083b9b4554758aed812a13f3231da
Ali Cenk Baytop <55930902+alicenkbaytop@users.noreply.github.com>
2020-01-22 11:19:42 +0300

    Update RecursiveFib.c

--- RecursiveFib.c
+++ RecursiveFib.c
@@ -2,9 +2,17 @@
 //By ACB
 int fib(int n)
 {
-	if (n >= 2)
+	if (n > 2)
 	{
 		return fib(n-1) + fib (n-2);
+	}
+	else if(n > 0 && n <= 2)
+	{
+		return 1;
+	}
+	else 
+	{
+		return printf("Error!!!!");
 	}
 }
 
