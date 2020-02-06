db72aff0f6b99c0250caa2fa6864c66623f76ea9
Ali Cenk Baytop <55930902+alicenkbaytop@users.noreply.github.com>
2020-02-06 16:38:44 +0300

    Update ArithmeticOperationsFunc.c

--- ArithmeticOperationsFunc.c
+++ ArithmeticOperationsFunc.c
@@ -21,11 +21,15 @@ int main()
 	int n1, n2;
 	printf("Enter two numbers:\n");
 	scanf("%d %d", &n1, &n2);
+	int add = addition(n1,n2);
+	int sub = subtraction(n1,n2);
+	int div = division(n1,n2);
+	int mul = multiplication(n1,n2);
 	
-	printf("addition is %d\n",addition(n1,n2));
-	printf("subtraction is %d\n",subtraction(n1,n2));
-	printf("division is %d\n",division(n1,n2));
-	printf("multiplication is %d\n",multiplication(n1,n2));
+	printf("addition is %d\n",add);
+	printf("subtraction is %d\n",sub);
+	printf("division is %d\n",div);
+	printf("multiplication is %d\n",mul);
 
 	return 0;
 }
